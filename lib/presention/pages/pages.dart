b7036c4878e68569720a719221bc9001dab702d6
magnumfilterr <ahmadahidayat01@gmail.com>
2023-02-16 17:20:58 +0700

    update terbaru

--- lib/presention/pages/pages.dart
+++ lib/presention/pages/pages.dart
@@ -9,15 +9,15 @@ import '../../model/costum_shapes.dart';
 part 'home/home_page.dart';
 
 part 'login/login_screen.dart';
-part 'registrasi/autentikasi.dart';
+part 'autentikasi/autentikasi.dart';
 part 'registrasi/registrasi.dart';
 part 'layanan/layanan_screen.dart';
-part 'login/config_pass_screen.dart';
-part 'login/success_pass_screen.dart';
-part 'login/lupa_password.dart';
-part 'tukar_poin/bank_sampah_screen.dart';
+part 'config_pass/config_pass_screen.dart';
+part 'success_password/success_pass_screen.dart';
+part 'lupa_password/lupa_password.dart';
+part 'bank_sampah/bank_sampah_screen.dart';
 part 'tukar_poin/tukar_poin1.dart';
 
-part 'registrasi/succes_screen.dart';
+part 'success_auntentikasi/succes_screen.dart';
 part 'splash_screen/splash_screen.dart';
 part 'navigasibar.dart';
